4fc828288b166fb2b062905c3adb77bd9dc1fe99
Tim Miller <innerlogic4321@gmail.com>
2015-06-07 12:23:45 -0400

    Add draft entity

---
+++ AwfulForumsLibrary/Entity/DraftEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Attributes;
+using SQLiteNetExtensions.Attributes;
+
+namespace AwfulForumsLibrary.Entity
+{
+    public class DraftEntity
+    {
+        [PrimaryKey]
+        public long Id { get; set; }
+
+        public long ThreadId { get; set; }
+
+        public string Draft { get; set; }
+
+        public int ForumId { get; set; }
+
+        [ManyToOne]
+        public ForumEntity Forum { get; set; }
+
+        public bool NewThread { get; set; }
+    }
+}
