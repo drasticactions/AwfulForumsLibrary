bf980484f504f758f7948b6c232fe04b7daa6164
Tim Miller <drasticactions@users.noreply.github.com>
2016-11-01 10:15:17 -0400

    Update AuthenticationManager.cs

--- AwfulForumsLibrary/Managers/AuthenticationManager.cs
+++ AwfulForumsLibrary/Managers/AuthenticationManager.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -24,8 +25,10 @@ namespace AwfulForumsLibrary.Managers
             using (var client = new HttpClient(handler))
             {
                 client.DefaultRequestHeaders.Referrer = new Uri("http://forums.somethingawful.com");
-                client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
-                client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2593.0 Safari/537.36");
+                client.DefaultRequestHeaders.Add("Accept",
+                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+                client.DefaultRequestHeaders.Add("User-Agent",
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2593.0 Safari/537.36");
                 client.DefaultRequestHeaders.Add("Accept-Language", "ja,en-US;q=0.8,en;q=0.6");
                 client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, sdch");
                 client.DefaultRequestHeaders.Add("Upgrade-Insecure-Requests", "1");
@@ -54,7 +57,8 @@ namespace AwfulForumsLibrary.Managers
                 AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
                 UseCookies = true,
                 UseDefaultCredentials = false,
-                CookieContainer = cookieContainer
+                CookieContainer = cookieContainer,
+                AllowAutoRedirect = false
             };
             using (var client = new HttpClient(handler))
             {
@@ -72,35 +76,40 @@ namespace AwfulForumsLibrary.Managers
                 var response = await client.PostAsync(new Uri(EndPoints.LoginUrl), header);
                 var authResult = new AuthResult()
                 {
-                    IsSuccess = response.IsSuccessStatusCode && cookieContainer.Count >= 3,
+                    IsSuccess = cookieContainer.Count >= 3,
                     AuthenticationCookie = cookieContainer
                 };
                 if (!checkResult)
                 {
                     return authResult;
                 }
-                if (cookieContainer.Count < 3)
+                try
                 {
-                    authResult.Error = "Cookie Container not set.";
+                    var location = "http:" + response.Headers.Location.OriginalString;
+                    var uri = new Uri(location);
+                    var queryString = Extensions.ParseQueryString(uri.Query);
+                    if (!queryString.ContainsKey("loginerror")) return authResult;
+                    if (queryString["loginerror"] == null) return authResult;
+                    switch (queryString["loginerror"])
+                    {
+                        case "1":
+                            authResult.Error = "Failed to enter phrase from the security image.";
+                            break;
+                        case "2":
+                            authResult.Error = "The password you entered is wrong. Remember passwords are case-sensitive! Be careful... too many wrong passwords and you will be locked out temporarily.";
+                            break;
+                        case "3":
+                            authResult.Error = "The username you entered is wrong, maybe you should try 'idiot' instead? Watch out... too many failed login attempts and you will be locked out temporarily.";
+                            break;
+                        case "4":
+                            authResult.Error =
+                                "You've made too many failed login attempts. Your IP address is temporarily blocked.";
+                            break;
+                    }
                 }
-                var queryString = Extensions.ParseQueryString(response.RequestMessage.RequestUri.Query);
-                if (!queryString.ContainsKey("loginerror")) return authResult;
-                if (queryString["loginerror"] == null) return authResult;
-                switch (queryString["loginerror"])
+                catch (Exception ex)
                 {
-                    case "1":
-                        authResult.Error = "Failed to enter phrase from the security image.";
-                        break;
-                    case "2":
-                        authResult.Error = "The password you entered is wrong. Remember passwords are case-sensitive! Be careful... too many wrong passwords and you will be locked out temporarily.";
-                        break;
-                    case "3":
-                        authResult.Error = "The username you entered is wrong, maybe you should try 'idiot' instead? Watch out... too many failed login attempts and you will be locked out temporarily.";
-                        break;
-                    case "4":
-                        authResult.Error =
-                            "You've made too many failed login attempts. Your IP address is temporarily blocked.";
-                        break;
+                    authResult.Error = ex.Message;
                 }
 
                 return authResult;
